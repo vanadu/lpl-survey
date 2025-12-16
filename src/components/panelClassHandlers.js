92b1d866a719791119a27c6af848f15b825317c8
Van Albert <motorhobo@gmail.com>
2025-12-16 05:58:41 -0500

    Minor refactor fixes

--- src/components/panelClassHandlers.js
+++ src/components/panelClassHandlers.js
@@ -1,35 +1,35 @@
 export function addCustomClasses(panel, htmlElement) {
     // Living Companion Details Panel and Subpanel
-    if (panel.name === 'livCompDetailsPanel') {
-      htmlElement.classList.add("liv-comp-details-panel");
+    if (panel.name === 'lvngCmpnDetailsPanel') {
+      htmlElement.classList.add("lvng-cmpn-details-panel");
     }
-    if (panel.name === 'livCompDetailsSubpanel1') {
-      htmlElement.classList.add("liv-comp-details-subpanel-1");
+    if (panel.name === 'lvngCmpnDetailsSubpanel1') {
+      htmlElement.classList.add("lvng-cmpn-details-subpanel-1");
     }
-    if (panel.name === 'livCompDetailsSubpanel2') {
-      htmlElement.classList.add("liv-comp-details-subpanel-2");
+    if (panel.name === 'lvngCmpnDetailsSubpanel2') {
+      htmlElement.classList.add("lvng-cmpn-details-subpanel-2");
     }
-    if (panel.name === 'livCompDetailsSubpanel3') {
-      htmlElement.classList.add("liv-comp-details-subpanel-3");
+    if (panel.name === 'lvngCmpnDetailsSubpanel3') {
+      htmlElement.classList.add("lvng-cmpn-details-subpanel-3");
     }
-    if (panel.name === 'livCompDetailsSubpanel4') {
-      htmlElement.classList.add("liv-comp-details-subpanel-4");
+    if (panel.name === 'lvngCmpnDetailsSubpanel4') {
+      htmlElement.classList.add("lvng-cmpn-details-subpanel-4");
     }
-    // Deceased Companion Details Panel and Subpanel
-    if (panel.name === 'decCompDetailsPanel') {
-      htmlElement.classList.add("dec-comp-details-panel");
+    // Deceased cmpnanion Details Panel and Subpanel
+    if (panel.name === 'dcsdCmpnDetailsPanel') {
+      htmlElement.classList.add("dcsd-cmpn-details-panel");
     }
-    if (panel.name === 'decCompDetailsSubpanel1') {
-      htmlElement.classList.add("dec-comp-details-subpanel-1");
+    if (panel.name === 'dcsdCmpnDetailsSubpanel1') {
+      htmlElement.classList.add("dcsd-cmpn-details-subpanel-1");
     }
-    if (panel.name === 'decCompDetailsSubpanel2') {
-      htmlElement.classList.add("dec-comp-details-subpanel-2");
+    if (panel.name === 'dcsdCmpnDetailsSubpanel2') {
+      htmlElement.classList.add("dcsd-cmpn-details-subpanel-2");
     }
-    if (panel.name === 'decCompDetailsSubpanel3') {
-      htmlElement.classList.add("dec-comp-details-subpanel-3");
+    if (panel.name === 'dcsdCmpnDetailsSubpanel3') {
+      htmlElement.classList.add("dcsd-cmpn-details-subpanel-3");
     }
-    if (panel.name === 'decCompDetailsSubpanel4') {
-      htmlElement.classList.add("dec-comp-details-subpanel-4");
+    if (panel.name === 'dcsdCmpnDetailsSubpanel4') {
+      htmlElement.classList.add("dcsd-cmpn-details-subpanel-4");
     }
 
 
