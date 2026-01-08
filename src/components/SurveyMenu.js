49af0a6729cfb51ac5167942a18adea68e85bb7b
Van Albert <motorhobo@gmail.com>
2026-01-08 10:41:22 -0500

    Reformat Master Survey Build header

--- src/components/SurveyMenu.js
+++ src/components/SurveyMenu.js
@@ -5,43 +5,43 @@ const SurveyHeader = () => {
   return (
     <>
 
-    <div class="survey-dropdown" id="myDropdown">
-      <button class="survey-dropdown__button">MASTER</button>
-      <ul class="survey-dropdown__list">
-        <li class="survey-dropdown__item"><Link href="/01_CmpnNameLifeStatus">
+    <div className="survey-dropdown" id="myDropdown">
+      <button className="survey-dropdown__button">MASTER</button>
+      <ul className="survey-dropdown__list">
+        <li className="survey-dropdown__item"><Link href="/01_CmpnNameLifeStatus">
           01_CmpnNameLifeStatus 
         </Link></li>
-        <li class="survey-dropdown__item"> <Link href="/02_UserInfo">
+        <li className="survey-dropdown__item"> <Link href="/02_UserInfo">
           02_UserInfo
         </Link></li>
-        <li class="survey-dropdown__item"><Link href="/03_InfoSources">
+        <li className="survey-dropdown__item"><Link href="/03_InfoSources">
           03_InfoSources
         </Link></li>
-        <li class="survey-dropdown__item"><Link href="/04_IntubationHistory">
+        <li className="survey-dropdown__item"><Link href="/04_IntubationHistory">
           04_IntubationHistory 
         </Link></li>
-        <li class="survey-dropdown__item"><Link href="/05_BreathingCrisis">
+        <li className="survey-dropdown__item"><Link href="/05_BreathingCrisis">
           05_BreathingCrisis 
         </Link></li>
-        <li class="survey-dropdown__item"><Link href="/06L_CmpnInfo">
+        <li className="survey-dropdown__item"><Link href="/06L_CmpnInfo">
           06L_CmpnInfo 
         </Link></li>
-        <li class="survey-dropdown__item"><Link href="/06L_CmpnInfo">
+        <li className="survey-dropdown__item"><Link href="/06L_CmpnInfo">
           06L_CmpnInfo 
         </Link></li>
-        <li class="survey-dropdown__item"><Link href="/07L_EarlySymptoms">
+        <li className="survey-dropdown__item"><Link href="/07L_EarlySymptoms">
           07L_EarlySymptoms 
         </Link></li>
-        <li class="survey-dropdown__item"><Link href="/08L_ChangedVets">
+        <li className="survey-dropdown__item"><Link href="/08L_ChangedVets">
           08L_ChangedVets 
         </Link></li>
-        <li class="survey-dropdown__item"><Link href="/09L_PrimaryDuration">
+        <li className="survey-dropdown__item"><Link href="/09L_PrimaryDuration">
           09_PrimaryDuration 
         </Link></li>
-        <li class="survey-dropdown__item"><Link href="/10L_PrimaryVet">
+        <li className="survey-dropdown__item"><Link href="/10L_PrimaryVet">
           10L_PrimaryVet
         </Link></li>
-        <li class="survey-dropdown__item">XXX</li>
+        <li className="survey-dropdown__item">XXX</li>
       </ul>
     </div>
 
@@ -51,46 +51,7 @@ const SurveyHeader = () => {
 
 
 
-{/* 
-      <div className="survey-links">
-        <Link href="/">
-          MASTER 
-        </Link>
-        <p>NEW</p>
-        <Link href="/01_CmpnNameLifeStatus">
-          01_CmpnNameLifeStatus 
-        </Link>
-        <Link href="/02_UserInfo">
-          02_UserInfo
-        </Link>
-        <Link href="/03_InfoSources">
-          03_InfoSources
-        </Link>
-        <Link href="/04_IntubationHistory">
-          04_IntubationHistory 
-        </Link>
-        <Link href="/05_BreathingCrisis">
-          05_BreathingCrisis 
-        </Link>
-        <Link href="/06L_CmpnInfo">
-          06L_CmpnInfo 
-        </Link>
-        <Link href="/07L_EarlySymptoms">
-          07L_EarlySymptoms 
-        </Link>
-        <Link href="/08L_ChangedVets">
-          08L_ChangedVets 
-        </Link>
-        <Link href="/09L_PrimaryDuration">
-          09_PrimaryDuration 
-        </Link>
-        <Link href="/10L_PrimaryVet">
-          10L_PrimaryVet
-        </Link>
-        <Link href="/dcsdroot">
-          dcsdRoot :-( 
-        </Link>
-      </div> */}
+
     </>
   )
 }
