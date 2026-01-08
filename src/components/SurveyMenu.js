7b383ebee4efd6df31bd1c25c32b6ac7e91b358b
Van Albert <motorhobo@gmail.com>
2026-01-08 09:39:53 -0500

    Added dropdown menu to SurveyMenu component and survey.scss

--- src/components/SurveyMenu.js
+++ src/components/SurveyMenu.js
@@ -4,7 +4,55 @@ import Link from 'next/link'
 const SurveyHeader = () => {
   return (
     <>
-      <div className="top-links">
+
+    <div class="survey-dropdown" id="myDropdown">
+      <button class="survey-dropdown__button">MASTER</button>
+      <ul class="survey-dropdown__list">
+        <li class="survey-dropdown__item"><Link href="/01_CmpnNameLifeStatus">
+          01_CmpnNameLifeStatus 
+        </Link></li>
+        <li class="survey-dropdown__item"> <Link href="/02_UserInfo">
+          02_UserInfo
+        </Link></li>
+        <li class="survey-dropdown__item"><Link href="/03_InfoSources">
+          03_InfoSources
+        </Link></li>
+        <li class="survey-dropdown__item"><Link href="/04_IntubationHistory">
+          04_IntubationHistory 
+        </Link></li>
+        <li class="survey-dropdown__item"><Link href="/05_BreathingCrisis">
+          05_BreathingCrisis 
+        </Link></li>
+        <li class="survey-dropdown__item"><Link href="/06L_CmpnInfo">
+          06L_CmpnInfo 
+        </Link></li>
+        <li class="survey-dropdown__item"><Link href="/06L_CmpnInfo">
+          06L_CmpnInfo 
+        </Link></li>
+        <li class="survey-dropdown__item"><Link href="/07L_EarlySymptoms">
+          07L_EarlySymptoms 
+        </Link></li>
+        <li class="survey-dropdown__item"><Link href="/08L_ChangedVets">
+          08L_ChangedVets 
+        </Link></li>
+        <li class="survey-dropdown__item"><Link href="/09L_PrimaryDuration">
+          09_PrimaryDuration 
+        </Link></li>
+        <li class="survey-dropdown__item"><Link href="/10L_PrimaryVet">
+          10L_PrimaryVet
+        </Link></li>
+        <li class="survey-dropdown__item">XXX</li>
+      </ul>
+    </div>
+
+
+
+
+
+
+
+{/* 
+      <div className="survey-links">
         <Link href="/">
           MASTER 
         </Link>
@@ -42,7 +90,7 @@ const SurveyHeader = () => {
         <Link href="/dcsdroot">
           dcsdRoot :-( 
         </Link>
-      </div>
+      </div> */}
     </>
   )
 }
