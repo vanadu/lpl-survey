f76e2a287b20453efccdcf869110c11add89e948
Van Albert <motorhobo@gmail.com>
2026-02-22 09:28:42 -0500

    2026.02.22B Mobile formattting start

--- src/components/SurveyMenu.js
+++ src/components/SurveyMenu.js
@@ -36,13 +36,13 @@ const SurveyHeader = () => {
 
   return (
     <div
-      className={`survey-dropdown ${open ? "open" : ""}`}
+      className={`survey-dev-menu ${open ? "open" : ""}`}
       ref={dropdownRef}
     >
       <button
         ref={buttonRef}
         type="button"
-        className="survey-dropdown__button"
+        className="survey-dev-menu__button"
         onClick={toggle}
         aria-haspopup="menu"
         aria-expanded={open}
@@ -51,107 +51,107 @@ const SurveyHeader = () => {
       </button>
 
       <ul
-        className="survey-dropdown__list"
+        className="survey-dev-menu__list"
         role="menu"
         aria-hidden={!open}
       >
-        <li className="survey-dropdown__item" role="none">
+        <li className="survey-dev-menu__item" role="none">
           <Link href="/00_Landing" onClick={close} role="menuitem">
             00_Landing
           </Link>
         </li>
 
-        <li className="survey-dropdown__item" role="none">
+        <li className="survey-dev-menu__item" role="none">
           <Link href="/01_UserInfo" onClick={close} role="menuitem">
             01_UserInfo
           </Link>
         </li>
 
-        <li className="survey-dropdown__item" role="none">
+        <li className="survey-dev-menu__item" role="none">
           <Link href="/02_CmpnInfo" onClick={close} role="menuitem">
             02_CmpnInfo
           </Link>
         </li>
 
-        <li className="survey-dropdown__item" role="none">
+        <li className="survey-dev-menu__item" role="none">
           <Link href="/03_InfoSources" onClick={close} role="menuitem">
             03_InfoSources
           </Link>
         </li>
 
-        <li className="survey-dropdown__item" role="none">
+        <li className="survey-dev-menu__item" role="none">
           <Link href="/04_IntubationHistory" onClick={close} role="menuitem">
             04_IntubationHistory
           </Link>
         </li>
 
-        <li className="survey-dropdown__item" role="none">
+        <li className="survey-dev-menu__item" role="none">
           <Link href="/05_BreathingCrisis" onClick={close} role="menuitem">
             05_BreathingCrisis
           </Link>
         </li>
 
-        <li className="survey-dropdown__item" role="none">
+        <li className="survey-dev-menu__item" role="none">
           <Link href="/06_EarlySymptoms" onClick={close} role="menuitem">
             06_EarlySymptoms
           </Link>
         </li>
 
-        <li className="survey-dropdown__item" role="none">
+        <li className="survey-dev-menu__item" role="none">
           <Link href="/07_PrimaryDuration" onClick={close} role="menuitem">
             07_PrimaryDuration
           </Link>
         </li>
 
-        <li className="survey-dropdown__item" role="none">
+        <li className="survey-dev-menu__item" role="none">
           <Link href="/08_PrimaryVet" onClick={close} role="menuitem">
             08_PrimaryVet
           </Link>
         </li>
 
-        <li className="survey-dropdown__item" role="none">
+        <li className="survey-dev-menu__item" role="none">
           <Link href="/09_Diagnosis" onClick={close} role="menuitem">
             09_Diagnosis
           </Link>
         </li>
 
-        <li className="survey-dropdown__item" role="none">
+        <li className="survey-dev-menu__item" role="none">
           <Link href="/10_TreatmentStatus" onClick={close} role="menuitem">
             10_TreatmentStatus
           </Link>
         </li>
 
-        <li className="survey-dropdown__item" role="none">
+        <li className="survey-dev-menu__item" role="none">
           <Link href="/11_TreatmentFactors" onClick={close} role="menuitem">
             11_TreatmentFactors
           </Link>
         </li>
 
-        <li className="survey-dropdown__item" role="none">
+        <li className="survey-dev-menu__item" role="none">
           <Link href="/12_Management" onClick={close} role="menuitem">
             12_Management
           </Link>
         </li>
 
-        <li className="survey-dropdown__item" role="none">
+        <li className="survey-dev-menu__item" role="none">
           <Link href="/13_OTCProducts" onClick={close} role="menuitem">
             13_OTCProducts
           </Link>
         </li>
 
-        <li className="survey-dropdown__item" role="none">
+        <li className="survey-dev-menu__item" role="none">
           <Link href="/14_Aspiration" onClick={close} role="menuitem">
             14_Aspiration
           </Link>
         </li>
 
-        <li className="survey-dropdown__item" role="none">
+        <li className="survey-dev-menu__item" role="none">
           <Link href="/15_Neuropathy" onClick={close} role="menuitem">
             15_Neuropathy
           </Link>
         </li>
 
-        <li className="survey-dropdown__item" role="none">
+        <li className="survey-dev-menu__item" role="none">
           <Link href="/16_Conclusion" onClick={close} role="menuitem">
             16_Conclusion
           </Link>
