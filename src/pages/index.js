5cbbc7f8d32329e1bca3a6b65444ed84b6e4a413
Van Albert <motorhobo@gmail.com>
2026-03-04 17:31:12 -0500

    First home page content and layout.

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,9 +27,9 @@ const Home = () => {
         canonical="https://larparlife.com"
         />
       <main class="page home">
-        <header class="home-hero card card--lift">
+        <header class="home-head card card--lift">
           <h1>Welcome to the 2026 LP/GOLPP Survey!</h1>
-          <p class="slug">Powered by larparlife.org</p>
+          <h2 class="slug">powered by larparlife.org</h2>
 
           <div class="start-survey-button-container">
             <a class="btn btn--primary start-survey-button" href="/survey">
@@ -44,26 +44,21 @@ const Home = () => {
         <section class="home-body">
           <article class="card">
             <p>
-              Did you know that the suicide rate among veterinarians is four times that
-              of the general population? Veterinarians have an impossible task – helping
-              patients who can’t tell them where it hurts...
+              Did you know that the suicide rate among veterinarians is four times that of the general population? Veterinarians have an impossible task &mdash; helping patients who can&lsquo;t tell them where it hurts. Euthanasia fatigue, high student debt, high burnout rates, difficult clients, and access to lethal medication are all cited as contributing factors. Trying to meet the unrealistic expectations of clients who are emotionally dependent on their companion animals for support is a Sisyphean task, to put it mildly.
             </p>
           </article>
 
           <article class="card">
             <h2>What does that have to do with this survey?</h2>
             <p>
-              As if their job wasn’t already difficult enough, veterinary medicine produces
-              more new research and continuing education than even the most committed vet
-              can keep up with...
+              Veterinary medicine produces more new research and continuing education than even the most committed vet can keep up with. Canine (or equine or feline) laryngeal paralysis have little in the way of empirical research and established protocols, so veterinarians often have to  &lsquo;wing it&rsquo; based on general veterinary-medical principles and professional judgement.  A well-designed survey can provide useful practical insights about the course of a disease over time based on the experience of people who are on the front lines in the trial-and-error battle against LP/GOLPP &mdash; people like you and me.
             </p>
           </article>
 
           <article class="card">
-            <h2>What’s the goal of this survey?</h2>
+            <h2>What&lsquo;s the goal of this survey?</h2>
             <p>
-              The goal of this survey is to capture long-term observations that veterinarians
-              rarely see, such as daily symptoms, medications, supplements...
+              The goal of this first-of-its-kind survey is to capture long-term observations that veterinarians might not see, such as daily symptoms, medications, supplements, or the aftermath of treatment. Your survey participation may shed light on treatment approaches that might otherwise fly below the veterinary radar. While it&lsquo;s not empirical research, this kind of aggregated real-world information can help veterinarians help their clients and the animals they love. And anything that helps veterinarians do their seemingly-impossible job is worth pursuing.
             </p>
           </article>
         </section>
